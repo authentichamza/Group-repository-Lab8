b1c14f9a1e9f62dd15773bbc837f0cea87fecd8f
hamza4465989 <43203240+hamza4465989@users.noreply.github.com>
2018-11-14 19:36:48 +0000

    Update Lab8Ex1.cpp

--- Lab8Ex1.cpp
+++ Lab8Ex1.cpp
@@ -10,70 +10,70 @@ private:
 
 public:
   Record(){
-    for(int i=0;i<11;i++){
+    for(int i=0;i<10;i++){
       record[i][0]=i;}
+      int i=0;
     for(int j=0;j<4;j++){
       record[0][i]=i+1;
     }
   }
   void input_grades(){
+    cout<<"Input grades"<<endl;
     for(int i=0;i<10;i++){
       for(int j=1;j<=3;j++){
         cin>>record[i][j];
       }
+      cout<<endl;
     }
   }
-  int total_marks(){
-    for(int i=0;i<=30;i++){
+  int totalMarks(){
+    for(int i=0;i<=10;i++){
       for(int j=1;j<=3;j++){
         total_marks+=record[i][j];
       }
-    return total_marks;
   }
+  cout<<total_marks<<endl;
 }
-  int average(){
+  int getAverage(){
     int i;
     cout<<"which student average required"<<endl;
     cin>>i;
-    return ((record[i][1]+record[i][2]+record[i][3])/3)
+    return ((record[i][1]+record[i][2]+record[i][3])/3);
   }
 
   int class_average()
   {
-    return class_average=total_marks/30;
+    return (total_marks/30);
   }
   int display_array(){
     for(int i=0;i<=10;i++){
       for(int j=1;j<=3;j++){
-        cout<<record[i][j];
+        cout<<record[i][j]<<" ";
       }
+      cout<<endl;
     }
   }
 };
 
 int main(int argc, char const *argv[]) {
-  Record std();
+  Record students;
   cout<<"Input Grades of Students for 3 Tests"<<endl;
-  std.input_grades();
-  std.display_array();
-  std.total_marks();
+  students.input_grades();
+  students.display_array();
+  students.totalMarks();
   cout<<"a if u want to access average of each student "<<endl;
   cout<<"b if you want to access class_average"<<endl;
   string choice;
   cin>>choice;
   for(int j=0;j<2;j++){
-  if(choice==a){
-  for(int i=1;i<=10;i++){
-  std.average();
+    if(choice=="a"){
+      for(int i=1;i<=10;i++){
+        students.getAverage();
+      }
+    }
+  if(choice=="b"){
+  students.class_average();
 }
 }
-  if(choice==b){
-  std.class_average();
-}
-}
-
-
-
-
   return 0;
 }
