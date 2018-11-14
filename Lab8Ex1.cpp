e24fb62bc9c403462de9de8fbed651c227724b10
hamza4465989 <43203240+hamza4465989@users.noreply.github.com>
2018-11-14 11:37:14 +0000

    Update Lab8Ex1.cpp

--- Lab8Ex1.cpp
+++ Lab8Ex1.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
+#include<string>
 using namespace std;
 class Record{
 private:
   int record[10][4];
   int average;
   int grades;
-  int total_marks=0;
+  int total_marks;
 
 public:
   Record(){
@@ -22,24 +23,57 @@ public:
       }
     }
   }
-  int total_marks(int record[][],int total_marks){
+  int total_marks(){
     for(int i=0;i<=30;i++){
       for(int j=1;j<=3;j++){
         total_marks+=record[i][j];
       }
+    return total_marks;
   }
-  int average(int record[][]){
+}
+  int average(){
     int i;
+    cout<<"which student average required"<<endl;
+    cin>>i;
     return ((record[i][1]+record[i][2]+record[i][3])/3)
   }
 
-  int class_average(int total_marks,int average)
+  int class_average()
   {
     return class_average=total_marks/30;
+  }
+  int display_array(){
+    for(int i=0;i<=10;i++){
+      for(int j=1;j<=3;j++){
+        cout<<record[i][j];
+      }
+    }
   }
 };
 
 int main(int argc, char const *argv[]) {
+  Record std();
+  cout<<"Input Grades of Students for 3 Tests"<<endl;
+  std.input_grades();
+  std.display_array();
+  std.total_marks();
+  cout<<"a if u want to access average of each student "<<endl;
+  cout<<"b if you want to access class_average"<<endl;
+  string choice;
+  cin>>choice;
+  for(int j=0;j<2;j++){
+  if(choice==a){
+  for(int i=1;i<=10;i++){
+  std.average();
+}
+}
+  if(choice==b){
+  std.class_average();
+}
+}
+
+
+
 
   return 0;
 }
